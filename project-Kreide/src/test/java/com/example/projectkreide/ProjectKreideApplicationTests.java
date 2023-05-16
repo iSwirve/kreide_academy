92a0d227796a16d03efa7d7f9ace1e2a8885f693
HaoD2 <73094389+HaoD2@users.noreply.github.com>
2023-05-16 20:15:09 +0700

    Main 1
    Awal Mula terbentuk

---
+++ project-Kreide/src/test/java/com/example/projectkreide/ProjectKreideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectkreide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKreideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
